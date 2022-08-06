5ca9c72ed48c006780658a213e7fd447f0015be2
João Lucas <j.lucassousa76@gmail.com>
2022-08-06 16:25:51 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import  {  saveAs  }  from  'file-saver' ;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,58 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'framwork';
+  propriedade:string = '';
+  nomeClasse:string = '';
+  listaPropriedades:Array<string> = [];
+  listaArquivosMontada:Array<string>;
+
+   escreverArquivo() {
+    this.listaArquivosMontada = [];
+    this.listaPropriedades.forEach((valor,index)=>{
+      const ultimaPosicao = this.listaPropriedades.length;
+      if(index == 0)
+      {
+        this.listaArquivosMontada.push("namespace");
+        this.listaArquivosMontada.push("{");
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push(`class name ${this.nomeClasse}`);
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push("{");
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push(`public string ${valor}`);
+      }
+      if(index !== 0 && index !== ultimaPosicao - 1)
+      {
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push(`public string ${valor}`);
+      }
+      if(index == ultimaPosicao - 1)
+      {
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push(`public string ${valor}`);
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push("}");
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push("}");
+      }
+    })
+    console.log(this.listaArquivosMontada)
+   setTimeout(() => {
+     let blob = new Blob(this.listaArquivosMontada,
+    {
+      type: "text/plain;charset=utf-8"
+    })
+    saveAs(blob, this.nomeClasse + ".txt");
+   }, 3000);
+    }
+
+    adicionaPropriedade(){
+      if(this.propriedade !== "")
+     this.listaPropriedades.push(this.propriedade)
+    }
+
+    removerPropriedade(index){
+      console.log(index)
+      this.listaPropriedades.splice(index,1);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+const componentsMaterial =[
+  MatInputModule,
+  MatCardModule,
+  MatIconModule,
+  MatListModule,
+  MatButtonModule
+]
 @NgModule({
+
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    componentsMaterial
   ],
   providers: [],
   bootstrap: [AppComponent]
