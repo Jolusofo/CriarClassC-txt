82a5988b35667c9af504bff586efff7654aaa6f3
João Lucas <j.lucassousa76@gmail.com>
2022-08-07 15:53:46 -0300

    atualização com metodos getset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,28 @@ export class AppComponent {
   listaPropriedades:Array<string> = [];
   listaArquivosMontada:Array<string>;
 
+
+  disabledBotaoGeraClass():boolean{
+  if(this.nomeClasse !== '')
+  {
+    if(this.listaPropriedades.length !== 0){
+      return false
+    }else{
+      return true
+    }
+  }else{
+    return true
+  }
+  }
+
    escreverArquivo() {
+
     this.listaArquivosMontada = [];
+    let montaMetodoGetSet:Array<string> = [];
+    montaMetodoGetSet = this.listaPropriedades;
     this.listaPropriedades.forEach((valor,index)=>{
       const ultimaPosicao = this.listaPropriedades.length;
+
       if(index == 0)
       {
         this.listaArquivosMontada.push("namespace");
@@ -26,40 +44,60 @@ export class AppComponent {
         this.listaArquivosMontada.push("\n");
         this.listaArquivosMontada.push("{");
         this.listaArquivosMontada.push("\n");
-        this.listaArquivosMontada.push(`public string ${valor}`);
+        this.listaArquivosMontada.push(`private string ${valor}`);
       }
       if(index !== 0 && index !== ultimaPosicao - 1)
       {
         this.listaArquivosMontada.push("\n");
-        this.listaArquivosMontada.push(`public string ${valor}`);
+        this.listaArquivosMontada.push(`private string ${valor}`);
       }
       if(index == ultimaPosicao - 1)
       {
         this.listaArquivosMontada.push("\n");
-        this.listaArquivosMontada.push(`public string ${valor}`);
+        index !==0? this.listaArquivosMontada.push(`private string ${valor}`) : '';
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push("\n");
+        this.listaArquivosMontada.push("\n");
+        montaMetodoGetSet.forEach(getSet =>{
+          this.listaArquivosMontada.push("\n");
+          this.listaArquivosMontada.push(`public void set${getSet[0].toUpperCase() + getSet.substring(1)}(string _${getSet}){ ${getSet} = _${getSet};}`);
+
+        });
+        this.listaArquivosMontada.push("\n");
+        montaMetodoGetSet.forEach(getSet =>{
+          this.listaArquivosMontada.push("\n");
+          this.listaArquivosMontada.push(`public string get${getSet[0].toUpperCase() + getSet.substring(1)}() { return ${getSet};}`);
+        });
+        this.listaArquivosMontada.push("\n");
         this.listaArquivosMontada.push("\n");
         this.listaArquivosMontada.push("}");
         this.listaArquivosMontada.push("\n");
         this.listaArquivosMontada.push("}");
       }
     })
-    console.log(this.listaArquivosMontada)
+
    setTimeout(() => {
      let blob = new Blob(this.listaArquivosMontada,
     {
       type: "text/plain;charset=utf-8"
     })
     saveAs(blob, this.nomeClasse + ".txt");
-   }, 3000);
+   }, 2000);
     }
 
     adicionaPropriedade(){
       if(this.propriedade !== "")
-     this.listaPropriedades.push(this.propriedade)
+      {
+        if(this.listaPropriedades.find(value => this.propriedade == value) == undefined)
+        this.listaPropriedades.push(this.propriedade)
+      }
+
+
+
     }
 
     removerPropriedade(index){
-      console.log(index)
+
       this.listaPropriedades.splice(index,1);
     }
 }
